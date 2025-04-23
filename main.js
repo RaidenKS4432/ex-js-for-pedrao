0ba2866d96647be85a8071478796cdff62f0cc52
RaidenKS4432 <kkll1100nzns@gmail.com>
2025-04-23 20:04:08 -0300

    ex 2 e ex 3

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 //  ex 1
-console.log(23 + 4);
-console.log(32 - 9);
-console.log(2 * 4);
-console.log(4 / 2);
-console.log(39 % 2);
+// console.log(23 + 4);
+// console.log(32 - 9);
+// console.log(2 * 4);
+// console.log(4 / 2);
+// console.log(39 % 2);
+
+// Escreva um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+// const number1 = 40;
+// const number2 = 799;
+// let biggerNumber;
+// if (number1 > number2) {
+//     biggerNumber = number1;
+// } else {
+//     biggerNumber = number2;
+// }
+// console.log('o maior numero é ' + biggerNumber);
+
+// Escreva um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+// const number1 = 20;
+// const number2 = 30;
+// const number3 = 10;
+// let biggerNumber;
+// if (number1 > number2 && number1 > number3) {
+//     biggerNumber = number1;
+// } else if (number2 > number1 && number2 > number3) {
+//     biggerNumber = number2;
+// } else if (number3 > number1 && number3 > number2) {
+//     biggerNumber = number3;
+// }
+// console.log(' o maior numero é ' + biggerNumber);
